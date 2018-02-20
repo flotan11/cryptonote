4451aeae5e280f5e2cfe450b77f8cb6ef942bd7f
root <root@vps48036.lws-hosting.com>
2018-02-20 14:08:03 +0000

    Active modification

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "MardonCoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[] = "013c01ff000109029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121019ef5a3fbb5c51c2eaf19485536e807f4c36e306e0db68b3a4b4239baa9c67f5e";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
